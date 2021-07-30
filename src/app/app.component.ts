c69e25e80eb1bd459648f2004bcb293c035b30bc
HpncharY <swiftmus12016@gmail.com>
2021-07-30 11:44:27 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Observable, observable} from "rxjs";
+import {Component,OnInit} from "@angular/core";
+import {elementAt} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'untitled';
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+      const generator = (text = "", possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") => {
+        for (let i = 0; i < 5; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length))
+        let palindrome = text.split('').reverse().join("")
+        if(palindrome == text){
+          return console.log("%c"+text,"color:red")
+        }
+        if(/^[0-9]+$/.test(text)){
+          return console.log("%c"+text,"color:blue")
+        }
+        if(text.includes("0")){
+          return ''
+        }
+        return text
+      }
+
+      setInterval(function (){
+        console.log(generator())
+      },3000)
+  }
+
+
 }
+
+
