a0f9209e7464a5d1402733123deb15315bcc1352
HpncharY <swiftmus12016@gmail.com>
2021-07-30 14:57:51 +0300

    finished test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,58 @@
-import {Observable, observable} from "rxjs";
-import {Component,OnInit} from "@angular/core";
-import {elementAt} from "rxjs/operators";
+import { Observable } from 'rxjs';
+import {Component} from "@angular/core";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
+  constructor() {
+    const generator = (text = "", possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") => {
+      for (let i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+      return text
+    }
 
-  ngOnInit() {
-      const generator = (text = "", possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") => {
-        for (let i = 0; i < 5; i++)
-          text += possible.charAt(Math.floor(Math.random() * possible.length))
-        let palindrome = text.split('').reverse().join("")
-        if(palindrome == text){
-          return console.log("%c"+text,"color:red")
-        }
-        if(/^[0-9]+$/.test(text)){
-          return console.log("%c"+text,"color:blue")
-        }
-        if(text.includes("0")){
-          return ''
-        }
-        return text
-      }
+    const observable = new Observable(subscriber => {
+        setInterval(() => {
+          subscriber.next(generator())
 
-      setInterval(function (){
-        console.log(generator())
-      },3000)
+        }, 3000)
+      });
+      observable.subscribe({
+
+        next(record){
+          const empty_plural = String(record)
+          const num = Number(String(record))
+          const palindrome = String(record).split("").reverse().join("")
+          const numEL = parseInt(palindrome.replace(/[^\d]/g,''))
+          if(empty_plural.includes("0")){
+            console.log("")
+          }
+          else if(num==numEL){
+            console.log("%c"+record,"color:blue")
+          }
+          else if(palindrome==record){
+            console.log("%c"+record,"color:red")
+          }
+          else if(/^[0-9]+$/.test(String(num))){
+            console.log("%c"+record,"color:blue")
+          }
+          else{
+            console.log(record)
+          }
+
+        },
+
+      });
+
+
+
+
+
   }
-
-
 }
-
 
